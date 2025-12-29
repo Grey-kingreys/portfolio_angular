0d94e667465da1a69a908c2f4ae1f0404d245a0b
unknown <soulmamoudou0@gmail.com>
2025-12-29 08:13:53 +0000

    Apres reprise de deployement

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { injectSpeedInsights } from '@vercel/speed-insights';
 
-// Initialise Vercel Speed Insights (mesure des performances côté client)
-injectSpeedInsights();
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
